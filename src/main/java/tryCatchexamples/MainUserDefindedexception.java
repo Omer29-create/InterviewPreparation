54ea82d3da20452682d5705fe35972e72286b24a
Ömer Aydin <64536242+Omer29-create@users.noreply.github.com>
2022-12-26 21:07:57 +0100

    Update MainUserDefindedexception.java

--- src/main/java/tryCatchexamples/MainUserDefindedexception.java
+++ src/main/java/tryCatchexamples/MainUserDefindedexception.java
@@ -2,9 +2,6 @@ package tryCatchexamples;
 
 public class MainUserDefindedexception {
 
-    
-    
-    
     public static void main(String[] args) {
         try {
             throw new UserDefinedException("This is my Exception");
