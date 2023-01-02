a1a1026d6efc76e3efd475b9e8c763d9ff213be8
Ömer Aydin <64536242+Omer29-create@users.noreply.github.com>
2023-01-02 16:01:31 +0100

    Update AltSinifOfAbstractClass1.java

--- src/main/java/abstractInterfaceUsage/AltSinifOfAbstractClass1.java
+++ src/main/java/abstractInterfaceUsage/AltSinifOfAbstractClass1.java
@@ -1,5 +1,26 @@
 package abstractInterfaceUsage;
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 public class AltSinifOfAbstractClass1 extends AbstractClass1 implements Interface1{
     @Override
     public void abstractMethod1() {
